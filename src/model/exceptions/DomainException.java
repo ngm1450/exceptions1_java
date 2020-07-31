387187776d9fb858177dccf363cbe58e52cdf1a8
Nicolas Georgeos Mantzos <georgeos.nicolas83@gmail.com>
2020-07-31 02:55:58 -0300

    Good solution

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,14 @@
+package model.exceptions;
+
+public class DomainException extends RuntimeException{
+
+	public DomainException(String msg) {
+		super(msg);
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
